a767dcc7cfb56bcd9902ccce403226724e5326f8
ARPIT _VAJRANGI <arpitvajrangi99@gmail.com>
2024-02-18 16:34:11 +0530

    test2

--- src/main/java/com/example/productserviceproject/controller/CategoryController.java
+++
@@ -1,7 +0,0 @@
-package com.example.productserviceproject.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CategoryController {
-}
--- src/main/java/com/example/productserviceproject/repositories/CategoryRepository.java
+++ src/main/java/com/example/productserviceproject/repositories/CategoryRepository.java
@@ -4,8 +4,12 @@ import com.example.productserviceproject.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 //use private entity of this interface in the service class-SelfProductService class
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
+    @Override
+    List<Category> findAll();
 }
